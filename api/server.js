d67cf0ed06f470b5deb67cdf366aba44df896648
Colin <cmarr768@gmail.com>
2019-01-22 21:23:31 -0500

    updating eslint testing hello world

--- api/server.js
+++ api/server.js
@@ -4,13 +4,11 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 
-const app = express.app();
-
-
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
 app.listen(PORT, HOST);
+
 console.log(`Running on http://${HOST}:${PORT}`);
